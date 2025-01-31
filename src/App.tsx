6134f72db2a97ad87d3b963eb5a63c8e59f8c30e
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-31 08:38:32 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import FrontPage from './components/FrontPage';
-import SavedPage from './pages/SavedPage';
+import SavedPage from './components/SavedPage';
 import { useJokes } from './hooks/useJokes';
 
 const App = () => {
